4b7fbe12291fa620a5aaccd2ad00cd0f9d26b4d6
Michael A. Cook <mcook0775@gmail.com>
2021-03-24 18:35:03 -0400

    Add .chunk

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-const _ = {}
+const _ = {
+  /**
+   * Take an array and divide into a 2D array of chunks of the specified size
+   * If there is any remainder, it will be at the end of the array
+   * @param {Object|Array} arr - input array to chunk
+   * @param {Number} size - chunk size
+   * @returns {Object|Array} output
+   */
+  chunk(arr, size) {
+    const output = []
+    while (arr.length > 0) {
+      output.push(arr.splice(0, size))
+    }
+    return output
+  },
+}
 
 module.exports = _
--- test/index.test.js
+++ test/index.test.js
@@ -1,3 +1,37 @@
 const { assert } = require("chai")
+const _ = require("../src/index.js")
 
-describe("_", () => {})
+describe("_", () => {
+  describe(".chunk", () => {
+    it("returns an array", () => {
+      const input = []
+      assert.isArray(_.chunk(input))
+    })
+
+    it("returns a 2D array with a length of 3", () => {
+      const input = ["a", "b", "c", "d", "e", "f"]
+      const expected = [
+        ["a", "b"],
+        ["c", "d"],
+        ["e", "f"],
+      ]
+      const actual = _.chunk(input, 2)
+      assert.deepEqual(actual, expected)
+    })
+
+    it("returns a 2D array with a remainder chunk with a length of 1", () => {
+      const input = ["a", "b", "c", "d", "e"]
+      const expected = [["a", "b"], ["c", "d"], ["e"]]
+      const actual = _.chunk(input, 2)
+      assert.deepEqual(actual, expected)
+    })
+
+    it("returns a 2D array with chunks containing only Number types", () => {
+      const input = [1, 2, 3]
+      const actual = _.chunk(input, 1)
+      actual.forEach((chunk) => {
+        assert.typeOf(chunk[0], "number", "is a number")
+      })
+    })
+  })
+})
