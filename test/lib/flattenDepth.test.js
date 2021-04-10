ec990798b968ab8c003759bd874a40d377b44097
Michael A. Cook <mcook0775@gmail.com>
2021-04-10 16:37:54 -0400

    Change name of method

--- test/lib/flattenDepth.test.js
+++ test/lib/flattenDepth.test.js
@@ -8,50 +8,50 @@ const {
 module.exports = () => {
   describe(".flattenNDeep", () => {
     it("returns an array", () => {
-      assert.isArray(_.flattenNDeep([]))
+      assert.isArray(_.flattenDepth([]))
     })
 
     it("returns an array flattened any level deep", () => {
       const input = [[["a", ["b", ["c"]]], "d"]]
       const expected = ["a", "b", "c", "d"]
-      const actual = _.flattenNDeep(input, 4)
+      const actual = _.flattenDepth(input, 4)
       assert.deepEqual(actual, expected)
     })
 
     it("returns an array flattened 1 level deep when passed no n argument", () => {
       const input = [[["a", ["b", ["c"]]], "d"]]
       const expected = [["a", ["b", ["c"]]], "d"]
-      const actual = _.flattenNDeep(input)
+      const actual = _.flattenDepth(input)
       assert.deepEqual(actual, expected)
     })
 
     it("flattens a deeply nested array of empty arrays to a single empty array", () => {
       const input = [[[[[[]]]]]]
       const expected = []
-      const actual = _.flattenNDeep(input, 6)
+      const actual = _.flattenDepth(input, 6)
       assert.deepEqual(actual, expected)
     })
 
     it("returns an array flattened to a single dimension when n is greater than the depth of the array", () => {
       const input = [[["a", ["b", ["c"]]], "d"]]
       const expected = ["a", "b", "c", "d"]
-      const actual = _.flattenNDeep(input, 10)
+      const actual = _.flattenDepth(input, 10)
       assert.deepEqual(actual, expected)
     })
 
     it("returns an empty array if passed an empty array", () => {
-      assert.isOk(!_.flattenNDeep([]).length)
+      assert.isOk(!_.flattenDepth([]).length)
     })
 
     it("throws a SyntaxError if arr is undefined", () => {
       assert.throw(() => {
-        _.flattenNDeep(undefined)
+        _.flattenDepth(undefined)
       }, MissingRequiredArgumentError.message)
     })
 
     it("throws a TypeError when passed a non-array type", () => {
       assert.throw(() => {
-        _.flattenNDeep("")
+        _.flattenDepth("")
       }, InvalidArgumentError.message)
     })
   })
